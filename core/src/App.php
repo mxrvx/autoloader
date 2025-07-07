5af3443fcfe214ad185a8728e15154b8d8fdf501
vgrish <vgrish@yandex.ru>
2025-07-07 13:47:57 +0300

    Add clearCache

--- core/src/App.php
+++ core/src/App.php
@@ -99,6 +99,56 @@ class App
         return $modx instanceof \modX ? $modx : \modX::getInstance();
     }
 
+    public static function clearCache(): void
+    {
+        self::deleteDirectory(MODX_CORE_PATH . 'cache/', true);
+    }
+
+    /**
+     * @see http://stackoverflow.com/questions/3349753/delete-directory-with-files-in-it
+     *
+     */
+    public static function deleteDirectory(string $directory, bool $contentOnly = false): bool
+    {
+        if (!\is_dir($directory)) {
+            return false;
+        }
+
+        /** @var \SplFileInfo[] $files */
+        $files = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($directory, \RecursiveDirectoryIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::CHILD_FIRST,
+        );
+
+        foreach ($files as $file) {
+            if ($file->isDir()) {
+                \rmdir($file->getRealPath());
+            } else {
+                self::deleteFile($file->getRealPath());
+            }
+        }
+
+        if (!$contentOnly) {
+            return \rmdir($directory);
+        }
+
+        return true;
+    }
+
+    public static function deleteFile(string $filename): bool
+    {
+        if (\file_exists($filename)) {
+            $result = \unlink($filename);
+
+            //Wiping out changes in local file cache
+            \clearstatcache(false, $filename);
+
+            return $result;
+        }
+
+        return false;
+    }
+
     public function getContainer(): Container
     {
         return $this->container;
